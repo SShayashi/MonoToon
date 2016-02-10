f51354833a498e362f3f47b12f44935506fc33d8
SShayashi <SShayashi0208@gmail.com>
2016-02-11 00:34:32 +0900

    HomeSceneの追加

---
+++ Classes/HomeScene.cpp
@@ -0,0 +1,9 @@
+//
+//  HomeScene.cpp
+//  MonoToon
+//
+//  Created by 林　真史 on 2016/02/11.
+//
+//
+
+#include "HomeScene.hpp"
---
+++ Classes/HomeScene.hpp
@@ -0,0 +1,14 @@
+//
+//  HomeScene.hpp
+//  MonoToon
+//
+//  Created by 林　真史 on 2016/02/11.
+//
+//
+
+#ifndef HomeScene_hpp
+#define HomeScene_hpp
+
+#include <stdio.h>
+
+#endif /* HomeScene_hpp */
