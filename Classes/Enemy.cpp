9db400ec79a63d5caf85f1cd7dbc6b062a1ca16c
SShayashi <SShayashi0208@gmail.com>
2016-02-12 00:50:01 +0900

    Enemyクラスを追加した

---
+++ Classes/Enemy.cpp
@@ -0,0 +1,9 @@
+//
+//  Enemy.cpp
+//  MonoToon
+//
+//  Created by 林　真史 on 2016/02/12.
+//
+//
+
+#include "Enemy.hpp"
---
+++ Classes/Enemy.hpp
@@ -0,0 +1,14 @@
+//
+//  Enemy.hpp
+//  MonoToon
+//
+//  Created by 林　真史 on 2016/02/12.
+//
+//
+
+#ifndef Enemy_hpp
+#define Enemy_hpp
+
+#include <stdio.h>
+
+#endif /* Enemy_hpp */
